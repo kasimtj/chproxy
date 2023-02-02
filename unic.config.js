42329db3d4e149874ea4116631157fadbd824005
Козлов Евгений Александрович <e.a.kozlov@tinkoff.ru>
2023-02-02 10:16:27 +0000

    Remove DEV2 Deploy

--- unic.config.js
+++ unic.config.js
@@ -56,60 +56,6 @@ module.exports = {
               "envs": {}
             },
           ],
-        },
-        "dev2": {
-          "name": "clickhouse-proxy-server-golang",
-          "type": "static",
-          "environment": "dev",
-          "namespace": "statist",
-          "services": [
-            {
-              "name": "app",
-              "resources": {
-                "replicas": 1,
-                "requests": {
-                  "cpu": '50m',
-                  "memory": '64Mi',
-                },
-                "limits": {
-                  "cpu": '50m',
-                  "memory": '64Mi',
-                },
-              },
-              "ports": {
-                "external": 80,
-                "internal": 8080
-              },
-              "probes": {
-                "liveness": {
-                  "path": '/metrics',
-                  "port": 8080
-                },
-                "readiness": {
-                  "path": '/metrics',
-                  "port": 8080
-                }
-              },
-              "extensions": [
-                {
-                  "kind": "metrics",
-                  "path": "/metrics",
-                  "port": 8080
-                },
-                {
-                  "kind": "notification",
-                  "agents": [
-                    {
-                      "type": "slack",
-                      "token": "$[ENV:SLACK_TOKEN]",
-                      "channel": "#statist-notify",
-                    }
-                  ]
-                }
-              ],
-              "envs": {}
-            }
-          ]
         }
       }
     }
